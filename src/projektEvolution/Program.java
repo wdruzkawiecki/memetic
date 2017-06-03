5ef682a50df2c36e870408fb306e141333afe4d6
student <ddd@ddd.dd>
2017-06-03 12:16:47 +0200

    Use JFrame in main class

--- src/projektEvolution/Program.java
+++ src/projektEvolution/Program.java
@@ -1,17 +1,26 @@
 package projektEvolution;
 
+import java.awt.EventQueue;
+
 public class Program {
-	public static void main(String args[]) {
+   public static void main(String args[]) {
 		
-		double[] point = new double[2];
-		point[0] = -1.0421;
-		point[1] = -1.0018;
-		Point p = new Point();
-		p.setVector(point);
-		
-		IFunction rastr = new RastriginFunction();
-		
-		System.out.println(rastr.getValue(p));
-	}
+//		double[] point = new double[2];
+//		point[0] = -1.0421;
+//		point[1] = -1.0018;
+//		Point p = new Point();
+//		p.setVector(point);
+//		
+//		IFunction rastr = new RastriginFunction();
+//		
+//		System.out.println(rastr.getValue(p));
+            
+                EventQueue.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        new MyFrame();
+                    }
+                });
+       }
 
 }
