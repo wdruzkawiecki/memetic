a7d169050ac0bd135abe0fc7e0aa2ab21168b41d
Krzysztof Kisiel <krzysiekkisiel@interia.pl>
2017-06-06 18:58:53 +0200

    Wstępny szkielet Algorytmu Ewolucyjnego

---
+++ src/projektEvolution/EvolutionAlgorithm.java
@@ -0,0 +1,33 @@
+package projektEvolution;
+
+public class EvolutionAlgorithm implements IAlgorithm {
+
+	public Population initialPopulation;
+	public Population resultPopulation;
+	
+	public void Mutation() {
+		
+	}
+
+	public void Crossover(Point p1, Point p2) {
+		double sigma1 = Math.random();
+		
+	}
+
+	public void SurvivorSelection() {
+		
+	}
+
+	public void StopCondition() {
+		
+	}
+
+	public void Run() {
+		
+	}
+
+	public void ReturnResultPopulation() {
+		
+	}
+
+}
